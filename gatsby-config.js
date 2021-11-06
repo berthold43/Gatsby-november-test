32506c4f6ceb627547539160fe34c5fb79b9fcab
berthold43 <thommy.johansson@sydnet.net>
2021-11-06 09:23:22 +0100

    Finish part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
